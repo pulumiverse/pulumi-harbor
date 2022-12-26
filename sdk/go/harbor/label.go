fabfbebbd05765e0733c35222d4d624d399f80fe
Engin Diri <engin.diri@ediri.de>
2022-12-26 19:38:56 +0100

    feat: import path fix

--- sdk/go/harbor/label.go
+++ sdk/go/harbor/label.go
@@ -19,7 +19,7 @@ import (
 // import (
 //
 //	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
-//	"github.com/pulumiverse/pulumi-harbor/sdk/go/harbor"
+//	"github.com/pulumiverse/pulumi-harbor/sdk/v3/go/harbor"
 //
 // )
 //
@@ -45,7 +45,7 @@ import (
 // import (
 //
 //	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
-//	"github.com/pulumiverse/pulumi-harbor/sdk/go/harbor"
+//	"github.com/pulumiverse/pulumi-harbor/sdk/v3/go/harbor"
 //
 // )
 //
